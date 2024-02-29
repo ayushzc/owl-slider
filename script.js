3d4a25c3967e3c3958ffc231b47a5a7913df9fc2
ayushzc <ayushdeyzc@gmail.com>
2024-02-29 15:21:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      dots: false,
+      lazyLoad: true,
+      responsiveClass:true
+    });
+  });
+  
+  $('.mynav-next').click(function() {
+      $(".owl-carousel").trigger('next.owl.carousel');
+  })
+  // Go to the previous item
+  $('.mynav-prev').click(function() {
+      $(".owl-carousel").trigger('prev.owl.carousel');
+  })
